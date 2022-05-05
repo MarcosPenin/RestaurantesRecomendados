46da7ab62efc70eb08bb0b9739d128684d0499ae
Marcos González Penín <a20marcosgp@ateis.local>
2022-05-05 19:46:05 +0200

    Diseño interfaz

---
+++ main.py
@@ -0,0 +1,35 @@
+import var
+import ventanaRest
+import windowaviso
+from PyQt5 import QtWidgets
+import sys
+
+
+class Main(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Main, self).__init__()
+        var.ui = ventanaRest.Ui_Proxecto1()
+        var.ui.setupUi(self)
+
+
+
+class DialogSalir(QtWidgets.QDialog):
+    def __init__(self):
+        super(DialogSalir, self).__init__()
+        var.dlgSlr = windowaviso.Ui_Dialog()
+        var.dlgSlr.setupUi(self)
+
+
+
+
+class FileDialogAbrir(QtWidgets.QFileDialog):
+    def __init__(self):
+        super(FileDialogAbrir,self).__init__()
+
+
+if __name__ == '__main__':
+    app=QtWidgets.QApplication([])
+    window=Main()
+    var.dlgSlr=DialogSalir()
+    window.show()
+    sys.exit(app.exec())
---
+++ var.py
@@ -0,0 +1,10 @@
+global ui
+global dlgSlr
+global chkPago
+global dlgCalendar
+global pay
+global filedb
+global filedlgabrir
+global copia
+global rep
+filedb = 'clientes.db'
